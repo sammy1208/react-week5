b4ba2a270c67a5a7bfa76d9e1bfd574f345c6bd7
sammy1208 <34804688+sammy1208@users.noreply.github.com>
2025-02-06 16:39:43 +0800

    改路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/react-week4/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/react-week5/" : "/",
   plugins: [react()],
 })
